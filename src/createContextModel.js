a331e0598f29db2f6c87ffeb80217366fb3b7869
takion <jo@surikat.pro>
2019-01-28 08:00:32 +0100

    merge runtime

--- src/createContextModel.js
+++ src/createContextModel.js
@@ -1,4 +1,5 @@
 import React, {createContext} from 'react'
+import deepmerge from 'deepmerge'
 
 import { Provider } from 'react-redux'
 
@@ -7,12 +8,35 @@ import createModel from './createModel'
 
 import { useContextStore, useContextAction } from '@redux-up/hooks'
 
+function mergeRuntime(callFunc, defaultModel = {}, defaultOptions = {}){
+  return function(model = {}, options = {}){
+    if(typeof model==='function'){
+      model = model(defaultModel)
+    }
+    else{
+      model = deepmerge(defaultModel, model)
+    }
+    if(typeof options==='function'){
+      options = options(defaultOptions)
+    }
+    else{
+      options = deepmerge(defaultOptions, options)
+    }
+    return callFunc(model, options)
+  }
+}
+
+
 function createContextModel(model, options){
   const context = createContext()
   return {
     context,
-    createModel: (m = model, o = options)=>createModel(m, o), //class constructor component
-    useModel: (m = model, o = options)=>useModel(m, o), //hook for function component
+    get createModel(){
+      return mergeRuntime(createModel, model, options)
+    }, //class constructor component
+    get useModel {
+      return mergeRuntime(useModel, model, options)
+    }, //hook for function component
     Provider: ({store, children}) => <Provider context={context} store={store} children={children} />,
     useStore: mapState => useContextStore(context, mapState),
     useAction: mapActions => useContextAction(context, mapActions),
